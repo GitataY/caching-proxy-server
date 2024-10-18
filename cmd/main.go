71c0c02847802a73d1ef4c0191ae033a2f03a927
Yvonne Gitata <yvonne.gitata@twiga.com>
2024-10-18 10:41:15 +0300

    WIP: takes user inputs and provides required flags
    Signed-off-by: Yvonne Gitata <yvonne.gitata@twiga.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "port to run the server on")
+	origin := flag.String("origin", "", "origin server url where requests are fowarded")
+	clearCache := flag.Bool("clear-cache", false, "clear cache")
+
+	flag.Parse()
+
+	if *clearCache {
+		fmt.Println("Clearing cache")
+		// function to clear cache here TODO
+		os.Exit(0)
+	}
+
+	if *origin == "" {
+		log.Fatal("Origin server url must be provided")
+	}
+
+	fmt.Printf("Starting caching proxy on port %d, forwarding to origin: %s\n", *port, *origin)
+}
